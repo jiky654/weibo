e23aa6f17855bf1dc1aa18282fdbe1f0dd6d46ab
liangminlong <>
2018-09-21 09:30:23 +0800

    testDev 提交 0001

--- testgit/testgit/ViewController.m
+++ testgit/testgit/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"测试,我是github");
+    NSLog(@"测试,我是github0001");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
